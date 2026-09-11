3621d13def048521555d9ea0e4f23f73d995abe9
Troach-07 <tsroach@mavs.coloradomesa.edu>
2026-09-11 15:30:33 +0000

    Pending changes exported from your codespace

---
+++ demo/demos.py
@@ -0,0 +1,11 @@
+print("hello world") 
+confused_hello="hello, World"
+print(confused_hello)
+
+name="punk"
+print(f"hello, {name}")
+print("hello",name)
+num=5 
+print(f"num is {num}")
+complete_string=f"num is {num}"
+print complete_string
